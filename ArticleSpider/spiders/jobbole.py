70c333aaffdd47760454b06ca7dc506d55a8495d
朱正浩 <arthur_zzh@126.com>
2018-01-30 22:32:29 +0800

    learn xpath selector

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://www.importnew.com/27940.html']
 
     def parse(self, response):
-        pass
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0]
+        pub_time_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0]
+        match_re = re.match('.*(\d{4}/\d{2}/\d{2})', pub_time_list)
+        if match_re:
+            pub_time = match_re.group(1)
+        content = response.xpath('//div[@class="entry"]').extract()[0]
+        print(content)
