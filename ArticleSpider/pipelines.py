d1f2a4b279eeeb027f03b622208d2862584f161a
朱正浩 <arthur_zzh@126.com>
2018-02-03 23:49:37 +0800

    add mysql

--- ArticleSpider/pipelines.py
+++ ArticleSpider/pipelines.py
@@ -4,8 +4,83 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import MySQLdb
+import MySQLdb.cursors
+from scrapy.exporters import JsonItemExporter
+from scrapy.pipelines.images import ImagesPipeline
+import codecs
+import json
 
 
 class ArticlespiderPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class JsonWithEncodingPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('article.json', 'w', encoding="utf-8")
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+class JsonExporterPipeline(object):
+    def __init__(self):
+        self.file = open('articleExport.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+
+class ArticleImagePipeline(ImagesPipeline):
+    def item_completed(self, results, item, info):
+        for ok, value in results:
+            image_file_path = value['path']
+        item['front_image_path'] = image_file_path
+        return item
+
+
+# class MysqlPipeline(object):
+#     def __init__(self):
+#         self.conn = MySQLdb.connect('localhost:3306', 'root', 'root', 'article_spider', charset='utf-8',
+#                                     use_unicode=True)
+#         self.cursor = self.conn.cursor()
+#
+#     def process_item(self, item, spider):
+#         insert_sql = """
+#             insert into article(title, url, create_date, fav_nums)
+#             VALUES (%s, %s, %s, %s)
+#         """
+#         self.cursor.execute(insert_sql, (item["title"], item["url"], item["create_date"], item["fav_nums"]))
+#         self.conn.commit()
+
+
+class MysqlPipeline(object):
+    # 采用同步的机制写入mysql
+    def __init__(self):
+        self.conn = MySQLdb.connect('127.0.0.1', 'root', 'root', 'article_spider', charset="utf8",
+                                    use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        insert_sql = """
+            insert into jobbole_article(url_object_id, title, url, create_date, content, front_image_url)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        self.cursor.execute(insert_sql,
+                            (item['url_object_id'], item["title"], item["url"], item["create_date"], item["content"],
+                             item["front_image_url"]))
+        self.conn.commit()
---
+++ ArticleSpider/utils/common.py
@@ -0,0 +1,15 @@
+# Created by Max on 2/3/18
+__author__ = 'Max'
+import hashlib
+
+
+def get_md5(url):
+    if isinstance(url, str):
+        url = url.encode('utf-8')
+    m = hashlib.md5()
+    m.update(url)
+    return m.hexdigest()
+
+
+if __name__ == '__main__':
+    print(get_md5('http://baidu.com'))
